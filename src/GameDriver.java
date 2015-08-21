42d1cbf4c51286c07b6d715df5117888333b4de4
Mohrnee <Ishan@Ishans-MacBook-Pro.local>
2015-08-20 23:08:42 -0700

    Made Frame in the Driver
    Subject to Change but I made the Frame in the driver for convenience’s
    sake and Basically added a jumping ball in the Panel to show jumping
    capabilities

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,10 +1,29 @@
+import javax.swing.JFrame;
+
 
 public class GameDriver {
 
+<<<<<<< Updated upstream
 	public static void main(String[] args) {
 		
 		GameFrame Frame = new GameFrame();
 		
+=======
+	public static void main(String[] args) throws InterruptedException{
+		JFrame Window = new JFrame("Simple Platformer");
+		GamePanel game = new GamePanel();
+		Window.setContentPane(game);
+		Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Window.setResizable(false);
+		Window.pack();
+		Window.setVisible(true);
+		
+        while (true) {
+        	game.moveBall();
+            game.repaint();
+            Thread.sleep(10);
+        }
+>>>>>>> Stashed changes
 	}
 	
 }
